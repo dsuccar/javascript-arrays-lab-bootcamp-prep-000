61aaf53f35e68587bc570e5377c6f023538c9a5a
Daniel Succar <danielsuccar@icloud.com>
2019-12-19 20:21:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 function destructivelyAppendKitten(name){
-   return destructivelyAppendKitten.push("name")
+   return kittens.push("name")
 
 }
 
