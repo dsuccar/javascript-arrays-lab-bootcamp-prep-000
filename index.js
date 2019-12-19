d03510092d1d2d2b218223a6840795f2737ae19f
Daniel Succar <danielsuccar@icloud.com>
2019-12-19 20:41:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,7 @@ function destructivelyRemoveFirstKitten(){
 function appendKitten(name){
   return [...kittens,'Broom']
 }
+function prependKitten(name){
+  return [...kittens,name]
+}
 // Add your functions and code here
