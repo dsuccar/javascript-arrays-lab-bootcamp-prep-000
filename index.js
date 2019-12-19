ad3077aa27a997aa0c728981a0252b1ab62b4911
Daniel Succar <danielsuccar@icloud.com>
2019-12-19 20:16:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
-var kittens[4]="Ralph"
+
+function destructivelyAppendKitten(name){
+  return [kittnes,'name']
+}
+
+
 // Add your functions and code here
